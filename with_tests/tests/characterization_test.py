7257f73aa44a545ec97f58b82e250d38829c5dab
Moussa <moutai10@gmail.com>
2020-10-04 15:41:11 -0400

    refactored training section

--- with_tests/tests/characterization_test.py
+++ with_tests/tests/characterization_test.py
@@ -2,8 +2,8 @@ from with_tests.testable_titanic_ml_pipeline import run_pipeline
 
 
 def test_functional_when_running_existing_code_the_pipeline_should_produce_known_output():
-    output, models = run_pipeline()
+    output, models_scores = run_pipeline()
     assert len(output.index) == 199
     assert set(output.columns.values) == {'PassengerId', 'Survived'}
     assert output['Survived'].mean() > 0.35
-    assert models['Score'].max() > 80
+    assert models_scores['Score'].max() > 80
